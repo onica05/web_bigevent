206f53d5cfb859d1da3a633d40d18ef877391c2f
monica <1650019033@qq.com>
2023-04-06 12:01:25 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,14 +7,16 @@
 // 注意： 每次调用$.get(),$.post(),$.ajax()的时候，会先调用ajaxPrefilter这个函数。在这个函数中，可以拿到我们给ajax提供的配置对象options
 $.ajaxPrefilter(function(options) {
 	
-    if (options.url.startsWith('/my')) {
+	// 统一为有权限的接口，设置headers请求头
+    if (options.url.startsWith('/my') !== -1) {
 		
+		/* 以 /my 开头的请求路径，需要在请求头中携带 Authorization 身份认证字段，才能正常访问成功 */
         options.headers = { Authorization: localStorage.getItem('token') || '' }
-
+        // 控制用户的访问权限——为了避免直接进入index.html页面
         options.complete = function(res) {
             if (res.responseJSON.status === 1 && res.responseJSON.msg === '身份认证失败') {
                 localStorage.removeItem('token')
-                location.href = '/login.html'
+                location.href = 'login.html'
             }
         }
     }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,24 @@
 $(function() {
-    getUserInfo()
+    getUserInfo()  /* 调用基本信息函数*/
 
     $('#btnLogout').on('click', function() {
+		/* 提示用户是否确定退出 */
         layui.layer.confirm('确定要退出吗？', {
             icon: 3,
             title: '提示',
             btn: ['确定', '取消']
         }, function(index) {
+			/* 清空本地存储的token */
             localStorage.removeItem('token')
-            location.href = '/login.html'
+			/* 重新跳转到登录页面 */
+            location.href = 'login.html'
+			/* layer.close() 官方自带。 关闭confirm询问框*/
             layui.layer.close(index)
         });
     })
 })
 
+/* 获取用户的基本信息 */
 function getUserInfo() {
     $('.layui-nav-img').hide()
     $.ajax({
@@ -21,24 +26,29 @@ function getUserInfo() {
         method: 'GET',
         success: function(res) {
             if (res.status !== 0) {
-                return layui.layer.msg(res.msg)
+                return layui.layer.msg(res.message) /* layer.msg()是提示框（和login那个一样）*/
             }
-            renderAvatar(res.data)
+			/* 调用这个函数 渲染用户的头像 */
+            renderAvatar(res.data) /* 用户的基本信息*/
         }
     })
 }
-
+/* 渲染头像 */
 function renderAvatar(user) {
     // 欢迎用户
-    var name = user.nickname || user.username
-    $('#welcome').html(`欢迎 ${name}`)
+    var name = user.nickname || user.username  /* 获取用户的名称*/
+    $('#welcome').html('欢迎&nbsp;&nbsp;'+ name)/* 设置欢迎的文本 */
 
     // 用户头像渲染
-    if (user.user_pic) {
-        $('.layui-nav-img').attr('src', user.user_pic).show()
+	
+	/* 按需渲染用户的头像，如有uesr有user_pic这个属性表示有设置头像，若没有则需要使用文本头像 */
+    if (user.user_pic !== null) {
+		// 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()  /* attr(attribute,value) 设置属性和值*/
         $('.text-avatar').hide()
     } else {
-        let first = name[0].toUpperCase()
+		// 渲染文本头像
+        let first = name[0].toUpperCase() /*获取字符串里的第一个字符并且转换为大写*/
         $('.text-avatar').html(first).show()
         $('.layui-nav-img').hide()
     }
